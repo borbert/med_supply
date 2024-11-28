73dadcd29c386d4bd20f351eb3f85eab19eaca16
Bob Owens <borbert@hotmail.com>
2024-11-28 12:17:05 -0500

    trying to resolve deploy errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,9 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <div className="text-5xl font-bold tracking-tight bg-gradient-to-r from-blue-600 to-cyan-500 bg-clip-text text-transparent">
+          MedSupply
+        </div>
         
         <h1 className="text-3xl font-bold text-center">
           Welcome to Your Application
